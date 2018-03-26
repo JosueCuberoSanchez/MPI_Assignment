6725b6a154d521a2d391379c3ddb7b887404312c
josue.cubero@ucr.ac.cr <josue.cubero@ucr.ac.cr>
2018-03-25 19:15:38 -0600

    llenadas

--- main.cpp
+++ main.cpp
@@ -11,6 +11,17 @@ using namespace std;
 int main(int argc,char **argv)
 {
     int myid, numprocs, numthreads, i;
+    int n; //n entered by the user
+    int M[n*n];
+    int V[n];
+    int Q[n];
+    int P[n];
+    int B[n*n];
+    int tp;
+    //data structures used by processes to calculate and store temporal data
+    bool columnPrimes[n]; //boolean vector to store if the ieth element is prime
+    int multiplicationResult; //result that will be used to create Q
+    int tpProcess; //total of primes for a specified process
 
     double startwtime, endwtime;
     int  namelen;
@@ -29,32 +40,29 @@ int main(int argc,char **argv)
     MPI_Barrier(MPI_COMM_WORLD);
 
     if (myid == 0) {
-        //data structures in general
-        int n; //n entered by the user
         cout << "Digite el n" << endl;
         cin >> n;
-        int M[n][n];
-        int V[n];
-        int Q[n];
-        int P[n];
-        int B[n][n];
-        int tp;
-        //data structures used by processes to calculate and store temporal data
-        bool columnPrimes[n]; //boolean vector to store if the ieth element is prime
-        int multiplicationResult; //result that will be used to create Q
-        int tpProcess; //total of primes for a specified process
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                M[i][j] = rand() % 9 + 1; //fill M with random ints from 0 to 9
+                M[i*n + j] = rand() % 9 + 1; //fill M with random ints from 0 to 9
             }
             V[i] = rand() % 4 + 1; //fill V with random ints from 0 to 5
         }
 
-        //do scatter
+        //do scatter: send n/p+[1|2] rows, V and n.
 
     }
 
+    //The next data structures are tests, because process should receive them from root
+    int row; //row number
+    int nReceived; //this is n, but root should send it to the process
+    if(row == 0 || row == n){
+        int rows[(numprocs/nReceived)+1]; //if it is first or last row, it will receive just an extra row.
+    } else {
+        int rows[(numprocs/nReceived)+2]; //if it is another row, it will receive 2 extra rows.
+    }
+    int vReceived[nReceived]; //V received
     //hacer operaciones
 
 
